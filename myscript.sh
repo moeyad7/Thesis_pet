62d3856da3828821bab2df49ec9119a6b5de9131
Mohamed Eyad <mohamed.eyad2612@gmail.com>
2023-10-27 13:01:13 +0200

    updated script

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export PATTERN_IDS=("4")
-export DATA_DIR="/Users/mohamedeyad/Documents/Thesis_Workspace/pet/data/CB/"
-export MODEL_TYPE="albert"
-export MODEL_NAME_OR_PATH="albert-xxlarge-v2"
-export TASK="cb"
-export OUTPUT_DIR="/Users/mohamedeyad/Documents/Thesis_Workspace/pet/output/"
+export PATTERN_IDS=(0 2 4)
+export DATA_DIR="/Users/mohamedeyad/Documents/Thesis_Workspace/Thesis_pet/data/BoolQ/"
+export MODEL_TYPE="roberta"
+export MODEL_NAME_OR_PATH="roberta-large"
+export TASK="boolq"
+export OUTPUT_DIR="/Users/mohamedeyad/Documents/Thesis_Workspace/Thesis_pet/output/"
