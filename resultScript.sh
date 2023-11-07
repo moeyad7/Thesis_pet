beb894020125fe461bc90189acb8c5c410328049
moeyad7 <mohamed.eyad2612@gmail.com>
2023-11-07 14:23:46 +0200

    fixed resultsScript issue

--- resultScript.sh
+++ resultScript.sh
@@ -61,25 +61,43 @@ for entry in "${data[@]}"; do
 done
 
 
-# # Loop through the associative array and print its contents
-# for key in "${!metric_data[@]}"; do
-#     value="${metric_data[$key]}"
-#     echo "Key: $key, Value: $value"
-# done
-
 declare -A combined_metrics
 
-# Loop through the metric_data associative array and combine values for keys that end with "-[0-9]"
+metric_name=()
+pattern_number=()
+pattern_value=()
+# #Loop through the metric_data associative array and combine values for keys that end with "-[0-9]"
 for key in "${!metric_data[@]}"; do
-    metric="${key%%-*}"  # Extract the metric part
-    value="${metric_data[$key]}"  # Get the value
+    value="${metric_data[$key]}"
     # Check if the key ends with "-[0-9]" using a regular expression
     if [[ "$key" =~ -[0-9]$ ]]; then
-        # Append the values for the same metric to the combined_metrics array
-        combined_metrics["$metric-p"]+=" $key:$value"
+        metric="${key%-*}"
+        number="${key##*-}"
+        metric_name+=("$metric")
+        pattern_number+=("$number")
+        pattern_value+=("$value")
     else
         # If the key doesn't end with "-[0-9]", store the value as is
         combined_metrics["$key"]="$value"
+    fi
+done
+
+# Initialize arrays to store combined values for ACC, recall, precision, and f1
+combined_f1=()
+combined_precision=()
+combined_recall=()
+combined_acc=()
+
+# Combine every two consecutive values for ACC, recall, precision, and f1
+for ((i = 0; i < ${#metric_name[@]}; i += 1)); do
+    if [ "${metric_name[i]}" == "acc" ]; then
+        combined_acc+=("${metric_name[i]}-${pattern_number[i]}: ${pattern_value[i]} ")
+    elif [ "${metric_name[i]}" == "recall" ]; then
+        combined_recall+=("${metric_name[i]}-${pattern_number[i]}: ${pattern_value[i]} ")
+    elif [ "${metric_name[i]}" == "precision" ]; then
+        combined_precision+=("${metric_name[i]}-${pattern_number[i]}: ${pattern_value[i]} ")
+    elif [ "${metric_name[i]}" == "f1" ]; then
+        combined_f1+=("${metric_name[i]}-${pattern_number[i]}: ${pattern_value[i]} ")
     fi
 done
 
@@ -87,14 +105,14 @@ done
 # Create the header for the CSV file
 header="f1-p,precision-p,recall-p,acc-p,f1-all,precision-all,recall-all,acc-all,final-f1,final-precision,final-recall,final-acc"
 
-
 # Create the CSV file with the specified format
 if [ ! -f "$output_csv" ]; then
   # If it doesn't exist, create the file with the header
   echo "$header" > "$output_csv"
 fi
 
-echo "\"${combined_metrics["f1-p"]}\",\"${combined_metrics["precision-p"]}\",\"${combined_metrics["recall-p"]}\",\"${combined_metrics["acc-p"]}\",\"${combined_metrics["f1-all"]}\",\"${combined_metrics["precision-all"]}\",\"${combined_metrics["recall-all"]}\",\"${combined_metrics["acc-all"]}\",\"${combined_metrics["final-f1"]}\",\"${combined_metrics["final-precision"]}\",\"${combined_metrics["final-recall"]}\",\"${combined_metrics["final-acc"]}\"" >> "$output_csv"
+echo "\"${combined_f1[@]}\",\"${combined_precision[@]}\",\"${combined_recall[@]}\",\"${combined_acc[@]}\",\"${combined_metrics["f1-all"]}\",\"${combined_metrics["precision-all"]}\",\"${combined_metrics["recall-all"]}\",\"${combined_metrics["acc-all"]}\",\"${combined_metrics["final-f1"]}\",\"${combined_metrics["final-precision"]}\",\"${combined_metrics["final-recall"]}\",\"${combined_metrics["final-acc"]}\"" >> "$output_csv"
 
 echo "Results saved to $output_csv"
 
+echo ${combined_metrics["final-acc"]}
