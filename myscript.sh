febd49d9a98b3292bc6fc766b07751a485d11b2a
moeyad7 <mohamed.eyad2612@gmail.com>
2023-11-08 20:51:54 +0200

    edit

--- myscript.sh
+++ myscript.sh
@@ -3,8 +3,8 @@
 current_dir="$(pwd)"
 
 export PATTERN_IDS=("0 1 2")
-export MODEL_TYPE="roberta"
-export MODEL_NAME_OR_PATH="roberta-base"
+export MODEL_TYPE="albert"
+export MODEL_NAME_OR_PATH="albert-base-v2"
 export TASK="ar-en-sa"
 export DATA_DIR="${current_dir}/data/ArEnSA/"
 export OUTPUT_DIR="${current_dir}/output/"
