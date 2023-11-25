ebab0e3b88461e48a8983a81786a7c59c6980887
moeyad7 <mohamed.eyad2612@gmail.com>
2023-11-25 16:26:00 +0200

    added new pattern for anercorp

--- pet/pvp.py
+++ pet/pvp.py
@@ -410,6 +410,8 @@ class ANERcorpPVP(PVP):
         
         if self.pattern_id == 0:
             return ['(',text_a,')',' ',text_b,':',self.mask], []
+        elif self.pattern_id == 1:
+            return [text_a,':',self.mask,'(',text_b,')'], []
         else:
             raise ValueError("No pattern implemented for id {}".format(self.pattern_id))
         
