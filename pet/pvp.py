1cfe52bffb3e706e2a8b1e63a04acff252324b5e
Mohamed Eyad <mohamed.eyad2612@gmail.com>
2023-12-22 16:48:37 +0200

    flipped punc patterns for myarsa

--- pet/pvp.py
+++ pet/pvp.py
@@ -488,26 +488,49 @@ class MyArSAPVP(PVP):
     def get_parts(self, example: InputExample) -> FilledPattern:
         text_a = self.shortenable(example.text_a)
         
+        # if self.pattern_id == 0:
+        #     return [text_a, ': ', self.mask], []
+
+        # elif self.pattern_id == 1:
+        #     return [text_a, '. ', self.mask], []
+
+        # elif self.pattern_id == 2:
+        #     return [text_a, ', ', self.mask], []
+
+        # elif self.pattern_id == 3:
+        #     return [text_a, '? ', self.mask], []
+
+        # elif self.pattern_id == 4:
+        #     return [text_a, '-', self.mask], []
+
+        # elif self.pattern_id == 5:
+        #     return [text_a, ' (', self.mask, ')'], []
+
+        # elif self.pattern_id == 6:
+        #     return [text_a, ' [', self.mask, ']'], []
+        
+        # Flipped
+        
         if self.pattern_id == 0:
-            return [text_a, ': ', self.mask], []
+            return [self.mask, ': ', text_a], []
 
         elif self.pattern_id == 1:
-            return [text_a, '. ', self.mask], []
+            return [self.mask, '. ', text_a], []
 
         elif self.pattern_id == 2:
-            return [text_a, ', ', self.mask], []
+            return [self.mask, ', ', text_a], []
 
         elif self.pattern_id == 3:
-            return [text_a, '? ', self.mask], []
+            return [self.mask, '? ', text_a], []
 
         elif self.pattern_id == 4:
-            return [text_a, '-', self.mask], []
+            return [self.mask, '-', text_a], []
 
         elif self.pattern_id == 5:
-            return [text_a, ' (', self.mask, ')'], []
+            return [self.mask, ' (', text_a, ')'], []
 
         elif self.pattern_id == 6:
-            return [text_a, ' [', self.mask, ']'], []
+            return [self.mask, ' [', text_a, ']'], []
         
         else:
             raise ValueError("No pattern implemented for id {}".format(self.pattern_id))
