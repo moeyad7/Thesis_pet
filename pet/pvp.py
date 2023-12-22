246b3ea9efec5cc1df1c191b42d4ad4e3a6c46c4
Mohamed Eyad <mohamed.eyad2612@gmail.com>
2023-12-22 14:57:06 +0200

    started punc patterns for arensa

--- pet/pvp.py
+++ pet/pvp.py
@@ -374,13 +374,26 @@ class ArEnSAPVP(PVP):
         text_a = self.shortenable(example.text_a)
         
         if self.pattern_id == 0:
-            return [self.mask,text_a], []
-        # Prompt patterns
+            return [text_a, ': ', self.mask], []
+
         elif self.pattern_id == 1:
-            return [self.mask,'الجملة السابقة؟',text_a], []
+            return [text_a, '. ', self.mask], []
+
         elif self.pattern_id == 2:
-            return [text_a,self.mask,'الجملة الاتية؟'], []
-        
+            return [text_a, ', ', self.mask], []
+
+        elif self.pattern_id == 3:
+            return [text_a, '? ', self.mask], []
+
+        elif self.pattern_id == 4:
+            return [text_a, '-', self.mask], []
+
+        elif self.pattern_id == 5:
+            return [text_a, ' (', self.mask, ')'], []
+
+        elif self.pattern_id == 6:
+            return [text_a, ' [', self.mask, ']'], []
+
         else:
             raise ValueError("No pattern implemented for id {}".format(self.pattern_id))
         
